4269a623284a5ba69282786e1b2534c41f9c548e
David Brigida <dmbrigida@gmail.com>
2019-11-30 12:23:16 -0500

    adding failure_count

--- analysis/failure_year.py
+++ analysis/failure_year.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 
 latest_data = pd.read_csv("./data/latest_data.csv")
@@ -5,4 +6,26 @@ latest_data = pd.read_csv("./data/latest_data.csv")
 # read data from pkl which keeps column type
 latest_data = pd.read_pickle("./data/latest_data.pkl")
 previous_data = pd.read_pickle("./data/previous_data.pkl")
+
+
+previous_data.fail_year.value_counts()
+latest_data.fail_year.value_counts()
+
+
+# see column names
+latest_data.columns.values
+
+# create only
+
+failure_count = latest_data["Closing Date"].value_counts()
+
+failure_count_df = pd.DataFrame(failure_count)
+
+# failure_count.set_index('Closing Date', inplace=True)
+
+failure_count_df.resample('QS').sum()
+
+
+            
+
 
